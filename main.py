91cb1b9fd2b7a67984d80d144e69e9ee275557fc
Tolu Owolabi <owolabitolu@gmail.com>
2026-03-24 10:23:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from scraper.scraper import scrape_books
+from scraper.parser import clean_data
+from scraper.validator import validate_data
+import pandas as pd
 
+def run_pipeline():
+    scrape_books()
+    df = clean_data()
+    validated = validate_data(df)
+
+    clean_df = pd.DataFrame(validated)
+    clean_df.to_csv("data/cleaned.csv", index=False)
+
+    print("Pipeline completed. Data saved.")
+
+
+if __name__ == "__main__":
+    run_pipeline()
